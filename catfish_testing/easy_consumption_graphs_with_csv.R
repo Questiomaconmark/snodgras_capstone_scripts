5ca2bfbca1e217a0530223eaf228c9634036ce60
Questiomaconmark <144583586+Questiomaconmark@users.noreply.github.com>
2025-12-07 20:12:26 -0700

    Made new script for easy selection of consumption graphs

---
+++ catfish_testing/easy_consumption_graphs_with_csv.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(exploreARTIS)
+
+current_fish <- "Grass_Carp"
+fish_data <- read.csv(paste0("c:/Users/John/bailey_stuff/snodgras_capstone_scripts/catfish_testing/csv_folder/", current_fish , "_Consumption.csv"))
+
+fish_data_filtered <- fish_data %>%
+  filter(source_country_iso3c == 'USA')
+
+fish_graph <- plot_ts(fish_data_filtered, artis_var = "consumer_iso3c", value = "consumption_live_t", prop_flow_cutoff = .01, plot.type = "stacked")
+
+print(fish_graph)
+
